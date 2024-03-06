130244a4412e9ee5662cb2bd8774476ad5885db3
”Moa <svensson.moaa@gmail.com>
2024-03-06 22:41:43 +0100

    Initial Commit

---
+++ Noted/NotedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotedApp.swift
+//  Noted
+//
+//  Created by Moa.Svensson on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct NotedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
