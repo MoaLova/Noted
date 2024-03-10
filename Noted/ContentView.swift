9b62a205e5b787c71487248b7382a0c8bbcda13f
”Moa <svensson.moaa@gmail.com>
2024-03-11 00:28:22 +0100

    plain list

--- Noted/ContentView.swift
+++ Noted/ContentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 import CoreData
 
+import SwiftUI
+import CoreData
+
 struct ContentView: View {
     @StateObject var viewModel = NotedViewModel()
 
     var body: some View {
         NavigationView {
             VStack {
-                
+              
                 List {
                     ForEach(viewModel.notes) { entity in
                         VStack {
@@ -27,16 +30,34 @@ struct ContentView: View {
                         viewModel.deleteNotes(indexSet: indexSet)
                     }
                 }
-                .listStyle(.plain)
+                .listStyle(.inset)
+
                 NavigationLink(destination: EditView(viewModel: viewModel)) {
                     Text("Add")
                 }
+                .padding() // Add padding for spacing
             }
-            .navigationTitle("Notes")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Spacer()
+                        Text("Noted")
+                            .font(.title) // Adjust the font size as needed
+                            .foregroundColor(.primary)
+                            .padding(.bottom, 8) // Add padding below the "Noted" text
+                        Spacer()
+                    }
+                }
+            }
         }
     }
 }
 
-#Preview {
-    ContentView()
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+#endif
